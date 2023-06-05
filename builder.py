a67fb0d76eb408ae54e376201e2c93d941bbe34e
Seongjun Park <pasuju1021@naver.com>
2023-06-05 16:42:44 +0900

    css part

--- builder.py
+++ builder.py
@@ -5,14 +5,20 @@ def make_table(dataframe):
     return [html.Table(
         [html.Thead(
             [html.Tr(
-                [html.Th(header_index, style={"borderBottom": "1px solid #444444", "padding": "10px"})
+                [html.Th(header_index, style={"padding": "10px"})
                  for header_index in dataframe.columns])
-             ]),
+             ], style={"position": "sticky", "top": "0px", "backgroundColor": "#111111", "borderBottom": "1px solid #444444"}),
          html.Tbody(
             [html.Tr(
                 [html.Td(data, style={"borderBottom": "1px solid #444444", "padding": "10px"})
                  for data in data_row]) for data_row in dataframe.values
              ])
-         ], id="country_total_daily", style={"width": "100%", "borderTop": "1px solid #444444", "borderCollapse": "collapse", "color": "white"}
+         ], id="country_total_daily", style={
+             "width": "100%",
+             "borderTop": "1px solid #444444",
+             "borderCollapse": "collapse",
+             "color": "white",
+
+        }
 
     )]
--- main.py
+++ main.py
@@ -20,19 +20,23 @@ app.layout = html.Div(
         html.Header(
             [html.H1("Corona Dashboard", style={
                 "fontSize": 50, "color": "white"})],
-            style={"textAlign": "center", "paddingTop": "50px"}
+            style={
+                "textAlign": "center",
+                "padding": "50px",
+                "border": "1px solid white"
+            }
 
         ),
         html.Div(
-            [html.Div(make_table(countries_df)
-
-
-                      )])
+            [html.Div(style={"width": "50%", "border": "1px solid white"}),
+             html.Div(make_table(countries_df), style={"height": "300px", "width": "100%", "overflow": "auto"})], style={"border": "1px solid white", "display": "flex"})
     ], style={
         "minHeight": "100vh",
         "backgroundColor": "#111111",
 
         "fontFamily": "Open Sans, sans-serif",
+
+
     }
 )
 
